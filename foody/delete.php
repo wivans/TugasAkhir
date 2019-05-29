5c6d11f97a92a9f65face39df5afd9b23dd7e0f5
Wahyu Ivan Satyagraha <ivan.satyagraha@gmail.com>
2019-05-29 22:28:38 +0700

    TARIK NAFAS LALU TIDUR

--- foody/delete.php
+++ foody/delete.php
@@ -8,4 +8,6 @@ $pass = "['".$entity."']";
 $command = escapeshellcmd("C:\\Users\\ASUS\\AppData\\Local\\Programs\\Python\\Python36\\python.exe C:\\xampp\\htdocs\\foody\\delete.py \"$pass\" \"$count\" ");
 exec($command, $output);
 
+
+
 ?>
